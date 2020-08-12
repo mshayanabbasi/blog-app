18c9d8c8a1302a28d1f3e1dfb249e2b9d536ba37
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-08-12 20:45:44 +0500

    Providers

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,22 @@
-import React from "react";
+import createDataContext from "./CreateDataContext";
 
-const BlogContext = React.createContext();
-
-export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogpost":
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
 };
+
+const addBlogPost = dispatch => {
+  return () => {
+    dispatch({ type: "add_blogpost" });
+  };
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
